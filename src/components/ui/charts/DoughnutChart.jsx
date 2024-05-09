d155952baee2c9043fbe8ced8ea338241cfebed8
Jack Tiew <jack960817@gmail.com>
2024-05-09 12:10:00 +0800

    Added chart color.

--- src/components/ui/charts/DoughnutChart.jsx
+++ src/components/ui/charts/DoughnutChart.jsx
@@ -21,8 +21,20 @@ const DoughnutChart = (props) => {
                                 {
                                     label: legend,
                                     data: data.map(x => x.value),
+                                    backgroundColor: [
+                                        "rgba(43, 63, 229, 0.8",
+                                        "rgba(250, 192, 19, 0.8",
+                                        "rgba(253, 135, 135, 0.8",
+                                        "rgba(129, 190, 171, 0.9)",
+                                        "rgba(148, 0, 129, 0.9)",
+                                        "rgba(196, 129, 193, 0.3)",
+                                        "rgba(91, 0, 5, 1)",
+                                        "rgba(116, 60, 223, 0.6)",
+                                        "rgba(7, 206, 21, 0.7)",
+                                        "rgba(210, 124, 45, 0.6)",
+                                    ]
                                 },
-                            ]
+                            ],
                         }}
                         options={{
                             plugins: {
